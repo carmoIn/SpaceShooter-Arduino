14f5568b687a9b794b5a15cda62ff9a30778757d
Helcio L. do Carmo Junior <helcio@carmo.in>
2021-06-10 14:27:53 -0300

    style: Traduz variáveis globais para inglês

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ void renderEnemy(uint8_t inimigo, uint16_t cor = ST77XX_WHITE);
 
 void updateProjectiles();
 void updateRanking();
-void enableEnemy(uint8_t inimigo, uint8_t posicaoX, uint8_t posicaoY);
+void enableEnemy(uint8_t inimigo, uint8_t positionX, uint8_t positionY);
 boolean shootPlayerProjectile();
 void updateEnemies();
 void removeEnemy(uint8_t inimigo);
@@ -81,7 +81,7 @@ void updatePlayerName(int8_t esquerda, int8_t direita);
 uint8_t checkEnemyCollision(uint8_t x, uint8_t y);
 
 
-const unsigned char nave[] PROGMEM = {
+const unsigned char playerShipBitmap[] PROGMEM = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00,
     0x00, 0x0d, 0x80, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00,
@@ -96,41 +96,41 @@ const unsigned char nave[] PROGMEM = {
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 typedef struct {
-    uint8_t posicaoX;
-    uint8_t posicaoY;
-    uint32_t ultimaAtualizacao;
-    boolean ativo;
+    uint8_t positionX;
+    uint8_t positionY;
+    uint32_t lastUpdate;
+    boolean active;
 } tiro;
 
 typedef struct {
-    uint8_t posicaoX;
-    uint8_t posicaoY;
-    uint32_t ultimoDano;
-    uint32_t ultimoMovimento;
-    uint8_t vida;
-    boolean ativo;
+    uint8_t positionX;
+    uint8_t positionY;
+    uint32_t lastDamageUpdate;
+    uint32_t lastMovementUpdate;
+    uint8_t hp;
+    boolean active;
 } inimigo;
 
-inimigo inimigos[MAX_ENEMIES] = {0, 0, 0, 0, 5, false};
-uint8_t totalInimigos = 0;
+inimigo enemies[MAX_ENEMIES] = {0, 0, 0, 0, 5, false};
+uint8_t totalEnemies = 0;
 
-tiro tiros[MAX_PLAYER_PROJECTILES] = {0, 0, 0, false};
-uint8_t totalTiros = 0;
+tiro playerProjectiles[MAX_PLAYER_PROJECTILES] = {0, 0, 0, false};
+uint8_t totalPlayerProjectiles = 0;
 
-uint8_t menuSelecionado = 1;
-uint8_t telaAtual = 0;
+uint8_t currentMenuOption = 1;
+uint8_t currentScreen = 0;
 
-uint8_t seletorNome = 0;
-char nomeJogador[MAX_NAME_PLAYER] = "A__";
-uint8_t totalVidas = MAX_PLAYER_LIVES;
-uint16_t totalPontos = 0;
-uint8_t posicaoNave = 100;
-uint32_t delayNave = 0;
+uint8_t playerNameSelector = 0;
+char playerName[MAX_NAME_PLAYER] = "A__";
+uint8_t totalLives = MAX_PLAYER_LIVES;
+uint16_t totalPoints = 0;
+uint8_t playerPositionX = 100;
+uint32_t lastPlayerMovementUpdate = 0;
 
-uint32_t delayTiro = 0;
+uint32_t lastPlayerShotUpdate = 0;
 
-uint32_t pontosRanking[MAX_RANKING_ENTRIES] = {0, 0, 0};
-char jogadores[MAX_RANKING_ENTRIES][MAX_NAME_PLAYER] = {"\0", "\0", "\0"};
+uint32_t rankingPoints[MAX_RANKING_ENTRIES] = {0, 0, 0};
+char playersNames[MAX_RANKING_ENTRIES][MAX_NAME_PLAYER] = {"\0", "\0", "\0"};
 
 void setup(void) {
     Serial.begin(9600);
@@ -186,8 +186,8 @@ void exibirHUD() {
     renderizarSimbolo(0, 20, GLYPH_HEART, BASE_TEXT_COLOR,0,1);
     tft.fillRect(24, 0, 75, 25, BACKGROUND_COLOR);
     formatBaseText(2);
-    showText(25, 5, String(totalVidas));
-    showText(55, 5, String(totalPontos));
+    showText(25, 5, String(totalLives));
+    showText(55, 5, String(totalPoints));
 }
 
 void showMainMenu() {
@@ -197,7 +197,7 @@ void showMainMenu() {
     formatBaseText(4);
     showText(36, FIRST_LINE_MENU + LINE_HEIGHT_MENU, F("Jogar"));
     showText(36, FIRST_LINE_MENU + (2 * LINE_HEIGHT_MENU), F("Ranking"));
-    showText(36, FIRST_LINE_MENU + (3 * LINE_HEIGHT_MENU), F("showCredits"));
+    showText(36, FIRST_LINE_MENU + (3 * LINE_HEIGHT_MENU), F("Creditos"));
     showText(36, FIRST_LINE_MENU + (4 * LINE_HEIGHT_MENU), F("Opcoes"));
 
     showMenuSelector();
@@ -206,23 +206,23 @@ void showMainMenu() {
 void hidePreviousSelector()
 {
     tft.setTextColor(BACKGROUND_COLOR);
-    showText(10, FIRST_LINE_MENU + (menuSelecionado * LINE_HEIGHT_MENU), F("\t"));   
+    showText(10, FIRST_LINE_MENU + (currentMenuOption * LINE_HEIGHT_MENU), F("\t"));   
     delay(20);
 }
 
 void showMenuSelector()
 {
     tft.setTextColor(BASE_TEXT_COLOR);
-    showText(10, FIRST_LINE_MENU + (menuSelecionado * LINE_HEIGHT_MENU), F("\t"));
+    showText(10, FIRST_LINE_MENU + (currentMenuOption * LINE_HEIGHT_MENU), F("\t"));
 }
 
 void updateMenuSelector()
 {
     hidePreviousSelector();
-    if (menuSelecionado < NUMBER_SCREENS) {
-        menuSelecionado++;
+    if (currentMenuOption < NUMBER_SCREENS) {
+        currentMenuOption++;
     } else {
-        menuSelecionado = SCREEN_PLAY;
+        currentMenuOption = SCREEN_PLAY;
     }
     showMenuSelector();
 
@@ -232,7 +232,7 @@ void updateMenuSelector()
 
 void selectMenuOption()
 {
-    switch (menuSelecionado) {
+    switch (currentMenuOption) {
         case SCREEN_PLAY: initGame();
             break;
         case SCREEN_RANKING: showRanking();
@@ -240,17 +240,17 @@ void selectMenuOption()
         case SCREEN_CREDITS: showCredits();
             break;
     }
-    telaAtual = menuSelecionado;
+    currentScreen = currentMenuOption;
 }
 
 void initGame()
 {
     clearScreen();
     delay(1000);  
-    totalPontos = 0;
-    seletorNome = 0;
-    strcpy(nomeJogador, "A__");
-    totalVidas = MAX_PLAYER_LIVES;
+    totalPoints = 0;
+    playerNameSelector = 0;
+    strcpy(playerName, "A__");
+    totalLives = MAX_PLAYER_LIVES;
 
     exibirHUD();
     renderPlayerShip();
@@ -273,15 +273,15 @@ void atualizaJogo(int8_t esquerda, int8_t direita)
 
 boolean shootPlayerProjectile()
 {
-    if (millis() - delayTiro > DELAY_SHOT_PROJECTILE) {
-        if (totalTiros < MAX_PLAYER_PROJECTILES) {
+    if (millis() - lastPlayerShotUpdate > DELAY_SHOT_PROJECTILE) {
+        if (totalPlayerProjectiles < MAX_PLAYER_PROJECTILES) {
             for (uint8_t i = 0; i < MAX_PLAYER_PROJECTILES; i++) {
-                if (!tiros[i].ativo) {
-                    tiros[i].posicaoX = posicaoNave + 14;
-                    tiros[i].posicaoY = 205;
-                    tiros[i].ultimaAtualizacao = 0;
-                    tiros[i].ativo = true;
-                    delayTiro = millis();
+                if (!playerProjectiles[i].active) {
+                    playerProjectiles[i].positionX = playerPositionX + 14;
+                    playerProjectiles[i].positionY = 205;
+                    playerProjectiles[i].lastUpdate = 0;
+                    playerProjectiles[i].active = true;
+                    lastPlayerShotUpdate = millis();
                     renderProjectile(i);
                     return true;
                 }
@@ -293,17 +293,17 @@ boolean shootPlayerProjectile()
 
 void renderProjectile(uint8_t tiro)
 {
-    tft.drawRect(tiros[tiro].posicaoX, tiros[tiro].posicaoY, 2, 5, BASE_TEXT_COLOR);
+    tft.drawRect(playerProjectiles[tiro].positionX, playerProjectiles[tiro].positionY, 2, 5, BASE_TEXT_COLOR);
 }
 
 void ocultarTiro(uint8_t tiro)
 {
-    tft.drawRect(tiros[tiro].posicaoX, tiros[tiro].posicaoY, 2, 5, BACKGROUND_COLOR);
+    tft.drawRect(playerProjectiles[tiro].positionX, playerProjectiles[tiro].positionY, 2, 5, BACKGROUND_COLOR);
 }
 
 void removerTiro(uint8_t tiro)
 {
-    tiros[tiro].ativo = false;
+    playerProjectiles[tiro].active = false;
     ocultarTiro(tiro);
 }
 
@@ -311,43 +311,43 @@ void updateProjectiles()
 {
     for(uint8_t i = 0; i < MAX_PLAYER_PROJECTILES; i++)
     {
-        if (tiros[i].ativo && (millis() - tiros[i].ultimaAtualizacao > DELAY_UPDATE_PROJECTILE)) {
+        if (playerProjectiles[i].active && (millis() - playerProjectiles[i].lastUpdate > DELAY_UPDATE_PROJECTILE)) {
             ocultarTiro(i);
-            if (tiros[i].posicaoY - MOVIMENTO_TIRO > 0) {
-                tiros[i].posicaoY -= MOVIMENTO_TIRO;
+            if (playerProjectiles[i].positionY - MOVIMENTO_TIRO > 0) {
+                playerProjectiles[i].positionY -= MOVIMENTO_TIRO;
             } else {
                 removerTiro(i);
                 return;
             }
             renderProjectile(i);
-            uint8_t inimigo = checkEnemyCollision(tiros[i].posicaoX, tiros[i].posicaoY);
+            uint8_t inimigo = checkEnemyCollision(playerProjectiles[i].positionX, playerProjectiles[i].positionY);
             if(inimigo != MAX_ENEMIES)
             {
                 addPoints(10);
                 removerTiro(i);
                 applyEnemyDamage(inimigo, 1);
             }
-            tiros[i].ultimaAtualizacao = millis();
+            playerProjectiles[i].lastUpdate = millis();
         }
     }
 }
 
 void applyEnemyDamage(uint8_t inimigo, uint8_t dano)
 {
-    if (inimigos[inimigo].vida - dano > 0)
+    if (enemies[inimigo].hp - dano > 0)
     {
-        inimigos[inimigo].vida -= dano;
-        inimigos[inimigo].ultimoDano = millis();
+        enemies[inimigo].hp -= dano;
+        enemies[inimigo].lastDamageUpdate = millis();
         renderEnemy(inimigo, ST77XX_RED);
     } else  {
-        inimigos[inimigo].vida = 0;
+        enemies[inimigo].hp = 0;
         removeEnemy(inimigo);
     }
 }
 
 void addPoints(uint16_t pontos)
 {
-    totalPontos += 10;
+    totalPoints += 10;
     exibirHUD();
 }
 
@@ -359,8 +359,8 @@ boolean checarColisaoCirculo(uint8_t alvoX, uint8_t alvoY, uint8_t raioAlvo, uin
 uint8_t checkEnemyCollision(uint8_t x, uint8_t y)
 {
     for (uint8_t i = 0; i < MAX_ENEMIES; i++) {
-        if (inimigos[i].ativo) {
-            if (checarColisaoCirculo(inimigos[i].posicaoX, inimigos[i].posicaoY, 25, x, y)) {
+        if (enemies[i].active) {
+            if (checarColisaoCirculo(enemies[i].positionX, enemies[i].positionY, 25, x, y)) {
                 return i;
             }
         }
@@ -368,15 +368,15 @@ uint8_t checkEnemyCollision(uint8_t x, uint8_t y)
     return MAX_ENEMIES;
 }
 
-void enableEnemy(uint8_t inimigo, uint8_t posicaoX, uint8_t posicaoY)
+void enableEnemy(uint8_t inimigo, uint8_t positionX, uint8_t positionY)
 {
-    if (!inimigos[inimigo].ativo) {
-        inimigos[inimigo].posicaoX = posicaoX;
-        inimigos[inimigo].posicaoY = posicaoY;
-        inimigos[inimigo].ultimoDano = 0;
-        inimigos[inimigo].ultimoMovimento = 0;
-        inimigos[inimigo].ativo = true;
-        inimigos[inimigo].vida = 5;
+    if (!enemies[inimigo].active) {
+        enemies[inimigo].positionX = positionX;
+        enemies[inimigo].positionY = positionY;
+        enemies[inimigo].lastDamageUpdate = 0;
+        enemies[inimigo].lastMovementUpdate = 0;
+        enemies[inimigo].active = true;
+        enemies[inimigo].hp = 5;
 
         renderEnemy(inimigo);
     }
@@ -384,13 +384,13 @@ void enableEnemy(uint8_t inimigo, uint8_t posicaoX, uint8_t posicaoY)
 
 void removeEnemy(uint8_t inimigo)
 {
-    inimigos[inimigo].ativo = false;
+    enemies[inimigo].active = false;
     hideEnemy(inimigo);
 }
 
 boolean checarRanking()
 {
-    if (totalPontos > pontosRanking[MAX_RANKING_ENTRIES - 1]) {
+    if (totalPoints > rankingPoints[MAX_RANKING_ENTRIES - 1]) {
         return true;
     }
     return false;
@@ -402,11 +402,11 @@ void imprimirTelaGameOver()
     showText(20, 60, F("GameOver"));
     formatBaseText(3);
     if (checarRanking()) {
-        telaAtual = SCREEN_SCORE;
-        showText(20, 100, String(totalPontos));
+        currentScreen = SCREEN_SCORE;
+        showText(20, 100, String(totalPoints));
         updateGameOverSelector();
     } else {
-        telaAtual = SCREEN_GAME_OVER;
+        currentScreen = SCREEN_GAME_OVER;
     }
 }
 
@@ -414,24 +414,24 @@ void updateGameOverSelector()
 {
     formatBaseText(3);
     tft.fillRect(20, 130, 55, 25, BACKGROUND_COLOR);
-    showText(20, 130, nomeJogador);
+    showText(20, 130, playerName);
 }
 
 void updatePlayerName(int8_t esquerda, int8_t direita)
 {
     checarRanking();
     if (esquerda == HIGH) {
-        if (nomeJogador[seletorNome] >= 'A' && nomeJogador[seletorNome] < 'Z') {
-            nomeJogador[seletorNome] += 1;
+        if (playerName[playerNameSelector] >= 'A' && playerName[playerNameSelector] < 'Z') {
+            playerName[playerNameSelector] += 1;
         } else {
-            nomeJogador[seletorNome] = 'A';
+            playerName[playerNameSelector] = 'A';
         }
         updateGameOverSelector();
     }
     if (direita == HIGH) {
-        if (seletorNome < 2) {
-            seletorNome++;
-            nomeJogador[seletorNome] = 'A';
+        if (playerNameSelector < 2) {
+            playerNameSelector++;
+            playerName[playerNameSelector] = 'A';
             updateGameOverSelector();
         } else {
             updateRanking();
@@ -443,38 +443,38 @@ void updatePlayerName(int8_t esquerda, int8_t direita)
 
 void updateRanking()
 {
-    strcpy(jogadores[MAX_RANKING_ENTRIES - 1], nomeJogador);
-    pontosRanking[MAX_RANKING_ENTRIES - 1] = totalPontos;
+    strcpy(playersNames[MAX_RANKING_ENTRIES - 1], playerName);
+    rankingPoints[MAX_RANKING_ENTRIES - 1] = totalPoints;
     sortRanking();
 }
 
 void perderVida()
 {
-    if (totalVidas <= 1) {
+    if (totalLives <= 1) {
         imprimirTelaGameOver();
     }
-    totalVidas--;
+    totalLives--;
     exibirHUD();
 }
 
 void updateEnemies()
 {
     for (uint8_t i = 0; i < MAX_ENEMIES; i++) {
-        if (inimigos[i].ativo && (millis() - inimigos[i].ultimoMovimento > 50)) {
+        if (enemies[i].active && (millis() - enemies[i].lastMovementUpdate > 50)) {
             hideEnemy(i);
-            if (inimigos[i].posicaoY + 4 > 220) {
-                inimigos[i].posicaoY = 0;
+            if (enemies[i].positionY + 4 > 220) {
+                enemies[i].positionY = 0;
 
                 perderVida();
             } else {
-                inimigos[i].posicaoY += 4;
+                enemies[i].positionY += 4;
             }
-            if (millis() - inimigos[i].ultimoDano > 150) {
+            if (millis() - enemies[i].lastDamageUpdate > 150) {
                 renderEnemy(i);
             } else {
                 renderEnemy(i, ST77XX_RED);
             }
-            inimigos[i].ultimoMovimento = millis();
+            enemies[i].lastMovementUpdate = millis();
         }
     }
 }
@@ -486,7 +486,7 @@ void hideEnemy(uint8_t inimigo)
 
 void renderEnemy(uint8_t inimigo, uint16_t cor)
 {
-    tft.fillCircle(inimigos[inimigo].posicaoX, inimigos[inimigo].posicaoY, 25, cor);
+    tft.fillCircle(enemies[inimigo].positionX, enemies[inimigo].positionY, 25, cor);
 }
 
 void showCredits()
@@ -494,7 +494,7 @@ void showCredits()
     clearScreen();
     formatBaseText(3);
 
-    showText(36, FIRST_LINE_MENU + LINE_HEIGHT_MENU, F("showCredits"));
+    showText(36, FIRST_LINE_MENU + LINE_HEIGHT_MENU, F("Creditos"));
 
     showText(36, FIRST_LINE_MENU + (2 * LINE_HEIGHT_MENU), F("Gabriel"));
     showText(36, FIRST_LINE_MENU + (3 * LINE_HEIGHT_MENU), F("Helcio"));
@@ -517,43 +517,43 @@ void showRanking()
 
     for (int i = 0; i < 3; i++) {
         tft.setCursor(36, FIRST_LINE_MENU + ((i + 2) * LINE_HEIGHT_MENU));
-        tft.print(jogadores[i]);
+        tft.print(playersNames[i]);
         tft.print (": ");
-        tft.print(pontosRanking[i]);
+        tft.print(rankingPoints[i]);
     }
 }
 
 void movePlayerShip(int8_t x)
 {
-    if (((x > 0 && posicaoNave < 215) ||
-        (x < 0 && posicaoNave > 0)) && (millis() - delayNave > 18)) {
+    if (((x > 0 && playerPositionX < 215) ||
+        (x < 0 && playerPositionX > 0)) && (millis() - lastPlayerMovementUpdate > 18)) {
         hidePlayerShip();
         
         if (x > 0) { // direita positivo
-            if (posicaoNave > 215) {
-                posicaoNave = 215;
+            if (playerPositionX > 215) {
+                playerPositionX = 215;
             }
-            posicaoNave += x;
+            playerPositionX += x;
         } else { // esquerda (negativo)
-            if(posicaoNave + x > 0) {
-                posicaoNave += x;
+            if(playerPositionX + x > 0) {
+                playerPositionX += x;
             } else {
-                posicaoNave = 0;
+                playerPositionX = 0;
             }
         }
-        delayNave = millis();
+        lastPlayerMovementUpdate = millis();
         renderPlayerShip();
     }
 }
 
 void renderPlayerShip()
 {
-    tft.drawBitmap(posicaoNave, 200, nave, 32, 32, ST77XX_WHITE);
+    tft.drawBitmap(playerPositionX, 200, playerShipBitmap, 32, 32, ST77XX_WHITE);
 }
 
 void hidePlayerShip()
 {
-    tft.drawBitmap(posicaoNave, 200, nave, 32, 32, BACKGROUND_COLOR);
+    tft.drawBitmap(playerPositionX, 200, playerShipBitmap, 32, 32, BACKGROUND_COLOR);
 }
 
 void sortRanking()
@@ -563,13 +563,13 @@ void sortRanking()
     char trocaNome[MAX_NAME_PLAYER];
     for (i = 0; i < MAX_RANKING_ENTRIES - 1; i++) {
         for (j = i + 1; j < 3; j++) {
-            if (pontosRanking[j] > pontosRanking[i]) {
-                troca = pontosRanking[j];
-                pontosRanking[j] = pontosRanking[i];
-                pontosRanking[i] = troca;
-                strcpy(trocaNome, jogadores[j]);
-                strcpy(jogadores[j], jogadores[i]);
-                strcpy(jogadores[i], trocaNome);
+            if (rankingPoints[j] > rankingPoints[i]) {
+                troca = rankingPoints[j];
+                rankingPoints[j] = rankingPoints[i];
+                rankingPoints[i] = troca;
+                strcpy(trocaNome, playersNames[j]);
+                strcpy(playersNames[j], playersNames[i]);
+                strcpy(playersNames[i], trocaNome);
             }
         }
     }
@@ -577,17 +577,17 @@ void sortRanking()
 
 void backMainMenu()
 {
-    telaAtual = SCREEN_MENU;
+    currentScreen = SCREEN_MENU;
     showMainMenu();
 }
 
 void loop() {
     int8_t selecionaEstado = digitalRead(BUTTON_SELECT_PIN);
     int8_t confirmaEstado = digitalRead(BUTTON_CONFIRM_PIN);
-    if (telaAtual == SCREEN_PLAY) {
+    if (currentScreen == SCREEN_PLAY) {
         atualizaJogo(selecionaEstado, confirmaEstado);
     } else {
-        if (telaAtual == SCREEN_MENU) {
+        if (currentScreen == SCREEN_MENU) {
             if (selecionaEstado == HIGH) {
                 updateMenuSelector();
             }
@@ -595,7 +595,7 @@ void loop() {
                 selectMenuOption();
             }
         } else {
-            if (telaAtual == SCREEN_SCORE) {
+            if (currentScreen == SCREEN_SCORE) {
                 updatePlayerName(selecionaEstado, confirmaEstado);
             } else {
                 if (selecionaEstado == HIGH || confirmaEstado == HIGH) {
